f4f42f5d85adb02b1a31f22a80ce0cff566d8940
hiro <khiro306@gmail.com>
2022-02-10 19:59:29 +0900

    本実装用のDBコンフィグ

--- dbconfig.php
+++ dbconfig.php
@@ -1,7 +1,7 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$db = "adranking";
-$password = "";
+$servername = "mysql202.phy.lolipop.lan";
+$username = "LAA1396240";
+$db = "LAA1396240-adremover";
+$password = "gyuudonn78";
 
