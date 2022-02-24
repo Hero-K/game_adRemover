a6b807bd76a2bd6602658951124ed1baed89db14
hiro <khiro306@gmail.com>
2022-02-24 13:12:54 +0900

    faviconを実装

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="robots" content="none">
+    <meta name="robots" content="none, noindex, nofollow">
+    <link rel="shortcut icon" href="images/favicon.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Hind:wght@400;500;700&family=M+PLUS+1:wght@300;400;700;900&display=swap" rel="stylesheet">
--- ranking.php
+++ ranking.php
@@ -20,7 +20,8 @@ $rankers = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="robots" content="none" />
+    <meta name="robots" content="none, noindex, nofollow" />
+    <link rel="shortcut icon" href="images/favicon.png">
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
